a0a500e9b8cb859cdf002933142c8c0c979580d8
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-12-15 19:08:40 -0500

    no estaba cargando las cosas del .env

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+source .env
+
 cd backend/
 npm install
 docker build -t app .
+
 docker run -p $WS_PORT:$WS_PORT -d -e WS_HOST=$WS_HOST -e WS_PORT=$WS_PORT --name app_c app
 
 cd ..
 cd frontend/
 npm install
 VITE_WS_HOST=$WS_HOST VITE_WS_PORT=$WS_PORT npm run build
-cp -r dist/* /var/www/html
-cp -r assets/* /var/www/html/src/assets
+sudo cp -r dist/* /var/www/html
+sudo cp -r src/assets/* /var/www/html/src/assets
+cd ..
